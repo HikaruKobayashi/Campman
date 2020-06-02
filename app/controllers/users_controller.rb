2de22b670a198d8627c6adf582d3e9eea9f28e69
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-06-02 23:05:12 +0900

    新規ユーザーの登録

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save!
+      session[:user_id] = @user.id
+      redirect_to("/")
+    else
+      render("/users/new")
+    end
+  end
+
+
+  def destroy
+    @user = User.find_by(id: params[:id])
+    @user.destroy
+    redirect_to('/users')
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end

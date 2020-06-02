5370bc9d51e3e28bd1480b37131c52c60187c7af
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-06-02 23:04:12 +0900

    ルーティングを設定。

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :relationships, only: [:create, :destroy]
+
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
+  get '/users' => 'users#new'
+  post '/users'=> 'users#create'
+  delete '/users' => 'users#destroy'
 end

bd17781d94cfdc2ee813a309be1768e3d56336f1
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-06-05 18:26:37 +0900

    フォロー、アンフォロー機能を作成。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,31 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  #フォローする
+  def follow
+    @user = User.find(params[:user_id])
+    current_user.follow(@user)
+    redirect_to user_path(@user)
+  end
+
+  #フォローを外す
+  def unfollow
+    @user = User.find(params[:user_id])
+    current_user.stop_following(@user)
+    redirect_to user_path(@user)
+  end
+
+
+  #フォローしてる人の一覧ページ
+  def follow_list
+    @user = User.find(params[:user_id])
+  end
+
+  #フォロワーの一覧ページ
+  def follower_list
+    @user = User.find(params[:user_id])
+  end
+
   def create
     @user = User.create(user_params)
     if @user.save!
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,25 +2,28 @@ class User < ApplicationRecord
   # usersテーブルにパスワードを保存する時にパスワードを暗号化させる。
   has_secure_password
 
-  has_many :relationships
-  has_many :followings, through: :relationships, source: :follow
-  has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
-  has_many :followers, through: :reverse_of_relationships, source: :user
+  acts_as_followable
+  acts_as_follower
 
-  def follow(other_user)
-    # フォローしようとしている人が自分自身でないことを確認する。
-    unless self == other_user
-      self.relationships.find_or_create_by(follow_id: other_user.id)
-    end
-  end
+  # has_many :relationships
+  # has_many :followings, through: :relationships, source: :follow
+  # has_many :reverse_of_relationships, class_name: 'Relationship', foreign_key: 'follow_id'
+  # has_many :followers, through: :reverse_of_relationships, source: :user
 
-  def unfollow(other_user)
-    # 既にフォローしていた場合にアンフォローにする。
-    relationship = self.relationships.find_by(follow_id: other_user.id)
-    relationship.destroy if relationship
-  end
+  # def follow(other_user)
+  #   # フォローしようとしている人が自分自身でないことを確認する。
+  #   unless self == other_user
+  #     self.relationships.find_or_create_by(follow_id: other_user.id)
+  #   end
+  # end
 
-  def following?(other_user)
-    self.followings.include?(other_user)
-  end
+  # def unfollow(other_user)
+  #   # 既にフォローしていた場合にアンフォローにする。
+  #   relationship = self.relationships.find_by(follow_id: other_user.id)
+  #   relationship.destroy if relationship
+  # end
+
+  # def following?(other_user)
+  #   self.followings.include?(other_user)
+  # end
 end

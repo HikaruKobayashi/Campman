6789000b45f40b0aeb7fa77a7f82487f2d3109a7
HikaruKobayashi <s-s-s2463@i.softbank.jp>
2020-06-05 18:26:11 +0900

    ルーティングを設定。

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,13 @@ Rails.application.routes.draw do
   root to: 'users#index'
 
   resources :users
-  resources :relationships, only: [:create, :destroy]
+  # resources :relationships, only: [:create, :destroy]
+
+  put 'users/follow/:user_id' => 'users#follow'
+  put 'users/unfollow/:user_id' => 'users#unfollow'
+
+  get 'users/follow_list/:user_id' => 'users#follow_list'
+  get 'users/follower_list/:user_id' => 'users#follower_list'
 
   get 'login' => 'sessions#new'
   post 'login' => 'sessions#create'
